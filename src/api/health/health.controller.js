ce353bf48501f28954152fab6bbef9e669a4044c
abirajs <meisterheidi89@gmail.com>
2025-05-05 15:13:19 +0530

    Delete src directory

--- src/api/health/health.controller.js
+++
@@ -1,7 +0,0 @@
-import httpUtils from '../../utils.js'
-
-function processRequest(request, response) {
-  httpUtils.sendResponse({ response, statusCode: 200 })
-}
-
-export default { processRequest }
--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import healthController from './api/health/health.controller.js'
-import paymentController from './api/payment/payment.controller.js'
-
-const routes = {
-  '/': paymentController.processRequest,
-  '/health': healthController.processRequest,
-}
-
-export { routes }
